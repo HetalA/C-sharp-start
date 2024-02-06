bdf625b1e793f866cff302cc69729314cfe4543a
Hetal Atwal <101687371+HetalA@users.noreply.github.com>
2024-02-06 15:34:41 +0530

    Delete Models directory

--- Models/StudentModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentApp.Models;
-
-public class Student
-{
-    public int Id { get; set; }
-    public string? Name { get; set;}
-    public DateTime Enrolldate {get; set;}
-    public string? Course { get; set;}
-}
